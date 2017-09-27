b13133cbe38a8870e6fda2fdefdb116af330f745
Barak Topaz <30661984+baraktopaz@users.noreply.github.com>
2017-09-27 21:38:35 +0300

    Add files via upload

---
+++ src/Car.java
@@ -0,0 +1,10 @@
+/**
+ * Created by hackeru on 9/27/2017.
+ */
+public class Car {
+    Human owner;
+    final Wheel [] wheels;
+    public Car(Wheel [] wheels){
+        this.wheels = wheels;
+    }
+}
